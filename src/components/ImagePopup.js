cf34359487303ab29a4d1fba472d7adcdd2686f9
Andrei Rahalevich <https://github.com/Andrei199729/Prech.git>
2022-01-15 22:10:43 +0300

    react console

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,7 +3,6 @@ import React from "react";
 function ImagePopup(props) {
     return (
         <div className={`popup popup_view-image popup_transition ${props.view ? 'popup_view-image_opened' : ''} `} onClick={props.onClose}>
-            {console.log(props.view)}
             <figure className="view-image">
                 <button className="popup__close-btn popup__close-btn_view-image" type="button" onClick={props.onClose}></button>
                 <img className="view-image__picture" src={props.card.link} alt={props.card.name} />
